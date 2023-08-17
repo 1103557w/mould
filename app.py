7ce687a254faca938eaef8d91ff5f6ed25d9a50d
1103557w <1103557w@student.gla.ac.uk>
2023-08-17 20:48:16 +0100

    added basic routing and html

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
 
+@app.route("result")
+def result():
+    return render_template("result.html")
+
+if __name__ == "__main__":
+    Flask.debug = True
+    Flask.run()
