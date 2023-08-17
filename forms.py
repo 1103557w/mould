92f1c840c67be000c96ce3f1f57b69045ce1fe6d
1103557w <1103557w@student.gla.ac.uk>
2023-08-17 21:30:40 +0100

    started rewriting for wtforms

---
+++ forms.py
@@ -0,0 +1,11 @@
+from wtforms import Form, IntegerField, RadioField, FloatField, SelectField
+from wtforms.validators import InputRequired, NumberRange
+
+
+class CalculatorForm(Form):
+    severity_choices = [("70%", .7), ("75%", 0.75), ("80%", 0.8), ("85%", 0.85), ("90%", 0.9), ("95%", 0.95), ("100%", 1)]
+    total_rooms = IntegerField(label="Total Number of Rooms: ", validators=[InputRequired(), NumberRange(1, 40)])
+    rooms_affected = IntegerField(label="Number of Rooms Effected: ", validators=[InputRequired(), NumberRange(1, 40)])
+    mould_months = IntegerField(label="Number of Months Mould Experienced: ", validators=[InputRequired(), NumberRange(1, 40)])
+    rent = FloatField(label="Rent Per Month in Pounds: ", validators=[InputRequired(), NumberRange(10, 10_000)])
+    severity = SelectField(label="Severity (See explanation): ", choices=severity_choices)
--- mould.py
+++ mould.py
@@ -6,7 +6,7 @@ mould = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@mould.route("/result")
+@mould.route("/result/", methods=["POST"])
 def result():
     return render_template("result.html")
 
