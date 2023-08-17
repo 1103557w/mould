dca2a934d5fda4399aee54cd25745759258ac2ae
1103557w <1103557w@student.gla.ac.uk>
2023-08-17 21:45:21 +0100

    continued rewriting for wtforms

--- mould.py
+++ mould.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from forms import CalculatorForm
 
 mould = Flask(__name__)
 
@@ -7,7 +8,17 @@ def index():
     return render_template("index.html")
 
 @mould.route("/result/", methods=["POST"])
-def result():
+def result(request):
+    form = CalculatorForm(request.post)
+    if request.method == "POST" and form.validate():
+        total_rooms = form.total_rooms.data
+        rooms_affected = form.rooms_affected.data
+        mould_months = form.rooms_affected.data
+        rent = form.rent.data
+        severity = form.severity.data
+        result = ((total_rooms * severity  * rent) / rooms_affected) * mould_months
+        flash(result)
+        return render_template("index.html")
     return render_template("result.html")
 
 if __name__ == "__main__":
