ea1e2754ae11d1a49863e209538bce0ad1ba99bc
1103557w <1103557w@student.gla.ac.uk>
2023-08-28 15:22:24 +0100

    commit before undoing

--- mould.py
+++ mould.py
@@ -9,17 +9,12 @@ def index():
 
 @mould.route("/result/", methods=["POST"])
 def result(request):
-    form = CalculatorForm(request.post)
-    if request.method == "POST" and form.validate():
-        total_rooms = form.total_rooms.data
-        rooms_affected = form.rooms_affected.data
-        mould_months = form.rooms_affected.data
-        rent = form.rent.data
-        severity = form.severity.data
-        result = ((total_rooms * severity  * rent) / rooms_affected) * mould_months
-        flash(result)
-        return render_template("index.html")
-    return render_template("result.html")
+    form = CalculatorForm(request.form)
+
+    form_data = {}
+    for field_name, field in form._fields.items():
+        form_data[field_name] = field.data
+    print(form_data)
 
 if __name__ == "__main__":
     mould.debug = True
