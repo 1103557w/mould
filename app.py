5d10c534f72a0c8a8ffc17bda7c161eb14fa6160
1103557w <1103557w@student.gla.ac.uk>
2023-08-28 21:56:38 +0100

    final commit before deploy

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, flash
 from forms import CalculatorForm
 
+
+# {': 12, : 2, : 2, '
+# rent': 132.0, 'severity': '0.7', 'csrf_token': 'IjY1ZDhkMGQzOTI5ZjMyMmIzYjA1NmQ4YWQyOGNkYWViMjdmYTE5ZmUi.ZOzJIg.7Dgh7KrtjgAkBmTZ55qm05OvwNU'}
 mould = Flask(__name__)
 
 mould.config.update(dict(
@@ -12,11 +15,15 @@ mould.config.update(dict(
 def index():
     form = CalculatorForm()
     if form.validate_on_submit():
-        # print(form.data)
-        mould.logger.warning("hey")
-        print("hey")
-        flash("HEY")
-        return render_template("index.html", form=form, result=12)
+        # TODO ensure rooms_affected <= rooms
+        rooms = form.total_rooms.data
+        rooms_affected = form.rooms_affected.data
+        mould_months = form.mould_months.data
+        rent = form.rent.data
+        severity = float(form.severity.data)
+        result_number = mould_months * (rooms_affected / rooms) * severity * rent
+        result_str = f"£{result_number:.2f}"
+        return render_template("index.html", form=form, result=result_str)
     return render_template("index.html", form=form)
 
 # @mould.route("/result/", methods=["POST"])
