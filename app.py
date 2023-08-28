8a3614a296202fa8484c104cc12c28be7dbbde43
1103557w <1103557w@student.gla.ac.uk>
2023-08-28 17:06:06 +0100

    getting there, don't seem to be entering the if tatement though?

--- app.py
+++ app.py
@@ -3,16 +3,27 @@ from forms import CalculatorForm
 
 mould = Flask(__name__)
 
-@mould.route("/")
+mould.config.update(dict(
+    SECRET_KEY="powerful secretkey",
+    WTF_CSRF_SECRET_KEY="a csrf secret key"
+))
+
+@mould.route("/", methods=["POST", "GET"])
 def index():
     form = CalculatorForm()
+    if form.validate_on_submit():
+        # print(form.data)
+        mould.logger.warning("hey")
+        print("hey")
+        flash("HEY")
+        return render_template("index.html", form=form, result=12)
     return render_template("index.html", form=form)
 
-@mould.route("/result/", methods=["POST"])
-def result(request):
-    form = CalculatorForm(request.form)
-
-    print(form.data)
+# @mould.route("/result/", methods=["POST"])
+# def result(request):
+#     form = CalculatorForm(request.form)
+#
+#     print(form.data)
 
 if __name__ == "__main__":
     mould.debug = True
